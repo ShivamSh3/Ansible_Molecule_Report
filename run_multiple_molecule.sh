fcb5950ba0b24dc5bb874327ee54eba9fb6eb952
ABHISHEK-SINHA10 <Abhishek.Sinha10@Dell.com>
2024-05-22 11:27:18 +0530

    Added code to create log folder if does not exist

--- run_multiple_molecule.sh
+++ run_multiple_molecule.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 # This shell script run each scenario in molecule directory and store log in log directory
-IWANTTORUN="cpu"
+REGEX_PATTERN="sensor"
 REPO_BASE_PATH="/root/collections/ansible_collections/dellemc/openmanage"
 BASE_CONFIG="/root/collections/ansible_collections/dellemc/openmanage/roles/molecule.yml"
 ENV_FILE_PATH="/root/.env.yml"
 current_file=$(readlink -f "$0")
 current_file_dir=$(dirname "$current_file")
 LOGPATH="$current_file_dir/log/"
+REPORT_GEN="$current_file_dir/report_generation.py"
+
 DIRECTORY_PATH_LIST="$(find $REPO_BASE_PATH -type d -name molecule)"
-DELETE_OLD_LOG="true"
+DELETE_OLD_LOG="false"
+
+if [ ! -d "$LOGPATH" ]; then
+  mkdir -p "$LOGPATH"
+fi
 
 if [[ "$DELETE_OLD_LOG" == "true" ]]; then
     find $LOGPATH -type f -name "*.log" -delete
@@ -24,11 +30,11 @@ do
         scenariofullpath="${scenario%/*}"
         each_scenario="${scenariofullpath##*/}"
         absolutelogpath="$LOGPATH$ROLENAME-$each_scenario.log"
-        if [[ -z "$IWANTTORUN" ]] || echo "$absolutelogpath" | grep -q $IWANTTORUN; then
+        if [[ -z "$REGEX_PATTERN" ]] || echo "$absolutelogpath" | grep -q $REGEX_PATTERN; then
             echo "Running command: molecule -vvv -e $ENV_FILE_PATH --base-config $BASE_CONFIG test -s $each_scenario > $absolutelogpath";
             (cd "$scenariopath" && molecule -vvv -e $ENV_FILE_PATH --base-config $BASE_CONFIG test -s $each_scenario > $absolutelogpath);
         fi
     done
 done
 
-python report_generation.py
+python $REPORT_GEN
